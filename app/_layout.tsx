2a388b91f1bfa7fe572b486f0536bd4321bb1164
Jace Phillips <jacedelynn@gmail.com>
2025-11-29 13:34:40 -0600

    setup expo with native wind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
-
+import "../global.css";
+import { Drawer } from "expo-router/drawer";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Drawer>
+      <Drawer.Screen
+        name="index"
+        options={{
+          drawerLabel: "Home",
+          drawerIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+          title: "Home",
+        }}
+      />
+    </Drawer>
+  );
 }
