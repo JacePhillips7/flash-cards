0ec5586fce71e6a967a179ccf818869ece54cbd5
Jace Phillips <jacedelynn@gmail.com>
2025-11-29 14:10:35 -0600

    added dialog

--- app/index.tsx
+++ app/index.tsx
@@ -6,16 +6,38 @@ import {
 } from "@/components/ui/card";
 import "../global.css";
 import { View } from "react-native";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Card>
-        <CardHeader>
-          <CardTitle>Create Deck</CardTitle>
-          <CardDescription>Create a new deck of flash cards</CardDescription>
-        </CardHeader>
-      </Card>
+      {createDeckButton()}
     </View>
+  );
+}
+function createDeckButton() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Card className="hover:bg-slate-200 hover:cursor-pointer">
+          <CardHeader>
+            <CardTitle>Create Deck</CardTitle>
+            <CardDescription>Create a new deck of flash cards</CardDescription>
+          </CardHeader>
+        </Card>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Deck</DialogTitle>
+        </DialogHeader>
+        Form goes here
+      </DialogContent>
+    </Dialog>
   );
 }
