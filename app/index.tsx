7c5795ed5c212382d8ae3666783b637f57e2d2fd
Jace Phillips <jacedelynn@gmail.com>
2025-11-29 13:43:41 -0600

    added shadcn

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,21 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import "../global.css";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create Deck</CardTitle>
+          <CardDescription>Create a new deck of flash cards</CardDescription>
+        </CardHeader>
+      </Card>
     </View>
   );
 }
